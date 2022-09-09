77a5b99280ef9e511f709eb0d960e08c82aa2b7e
Winnie0313 <liwanyi0313@gmail.com>
2022-09-09 16:19:38 -0700

    Implemented a function to get appointments for the day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+// returns an array of appointments for that day
 export function getAppointmentsForDay(state, day) {
-  const appointmentsArray = state.days
-  
-  //... returns an array of appointments for that day
+  const appointmentsArray = [];
+  // find the day object
+  const dayObj = state.days.find(item => item.name == day);
+
+  if (!dayObj) {
+    return [];
+  }
+  // find the appointment object with the same id
+  for (let id of dayObj.appointments) {
+    let appointment = state.appointments[id];
+    appointmentsArray.push(appointment);
+  }
+
+  return appointmentsArray;
 }
